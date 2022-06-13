03845a4ae665da4e608bebc71ad05bdbf69226d7
s-takesawa-necomata <s.takesawa@necomata.co.jp>
2022-06-13 14:15:12 +0900

    ログインUI作成 エラーメッセージの実装

---
+++ workspace/myapp/nuxt/components/Caption.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="caption" align="center">
+        <P></P>
+        <p class="attention">※ログインできるのは、ねこまた社員のみです。</p>
+    </div>
+</template>
+
+<style>
+.attention{
+    color:red;
+}
+</style>
